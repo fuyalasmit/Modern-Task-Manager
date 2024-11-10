0636c5de7818eee42df4715f94743ddf54bb4440
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-11-10 12:33:07 +0545

    drag

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import TaskForm from './components/TaskForm'
 import TaskColumn from './components/TaskColumn'
+import Intro from './components/Intro'
 import todoIcon from "./assets/direct-hit.png"
 import doingIcon from "./assets/glowing-star.png"
 import doneIcon from "./assets/check-mark-button.png"
@@ -42,7 +43,7 @@ const App = () => {
   return (
     <>
       <div className='app' >
-        
+        {/* <Intro/> */}
         <TaskForm setTasks={setTasks} />
         <main className='appMain flex flex-row justify-evenly py-[20px] px-[8%]' >
           <TaskColumn icon={todoIcon} columnTitle="To Do" tasks={tasks} status="todo" deleteHandler={deleteHandler} setActiveCard={setActiveCard} dropHandler={dropHandler} />
---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <nav className='bg-blue-400 h-fit px-44 py-5 text-center' >
+        Welcome to Modern Task Manager! Start by adding tasks, choosing relevant tags to organize them. You can easily delete tasks and also move tasks between sections (e.g. from 'To Do' to 'Doing') to track your progress.
+    </nav>
+  )
+}
+
+export default Intro
